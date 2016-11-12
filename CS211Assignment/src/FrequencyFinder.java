66a80ac6561eb37dd377655117d8d878e4dc1bcf
vCra <aaw13@aber.ac.uk>
2016-11-12 19:54:26 +0000

    Implamented HashMap and File Reading.

--- CS211Assignment/src/FrequencyFinder.java
+++ CS211Assignment/src/FrequencyFinder.java
@@ -1,6 +1,57 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by aaron@vcra.net on 10/11/16.
  */
 public class FrequencyFinder {
+    BufferedReader reader;
+    String textFileLocation = new String();
+    Map<Integer, Integer> charCount;
 
+    public void setTextFile(String file){
+        this.textFileLocation=file;
+    }
+
+    private void setReader(){
+        try{
+            this.reader = new BufferedReader(new InputStreamReader(new FileInputStream(this.textFileLocation)));
+        }
+        catch (FileNotFoundException e){e.printStackTrace();}
+    }
+
+    private void createMap(){
+        charCount = new HashMap<>();
+    }
+    private void file2map(){
+        int o;//Stores the current value of the object we are getting
+        try {
+            while ((o=reader.read())>0){
+                if(charCount.containsKey(o))
+                {
+                    charCount.put(o,charCount.get(o)+1); //If it exists in the map already, then get the current count and add 1 to it.
+                }
+                else //If it does not exist
+                {
+                    charCount.put(o, 1); //set the count as 1
+                }
+            }
+        }
+        catch (java.io.IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void run(){
+        long startTime = System.currentTimeMillis();
+        setTextFile("/test/shakespere.txt");
+        setReader();
+        createMap();
+        file2map();
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println(totalTime);
+
+        charCount.forEach((a,b)-> System.out.println((char) a.intValue()+", "+b));
+    }
 }
--- CS211Assignment/src/aaw13CS21120Assign.java
+++ CS211Assignment/src/aaw13CS21120Assign.java
@@ -2,5 +2,9 @@
  * Created by aaron@vcra.net on 10/11/16.
  */
 public class aaw13CS21120Assign {
+    public static void main(String[] args) {
+        FrequencyFinder app = new FrequencyFinder();
+        app.run();
 
+    }
 }
